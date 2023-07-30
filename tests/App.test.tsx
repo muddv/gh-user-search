65c6b626c28e049fc58acceefa7d7138cbb86c6f
misha <dedovmu@gmail.com>
2023-07-30 20:30:02 +0300

    Add tests

---
+++ tests/App.test.tsx
@@ -0,0 +1,86 @@
+import { describe, expect, it } from 'vitest';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Provider as JotaiProvider } from 'jotai';
+import { useHydrateAtoms } from 'jotai/utils';
+
+import { customRender } from './test-utils'
+import { App } from '../src/App';
+import { SearchResults } from '../src/SearchResulsts';
+import { Search } from '../src/Search';
+import { searchResAtom } from '../stores/searchRes';
+import { currentSearchParams } from '../lib/useSearch';
+
+type TestProviderProps = {
+  initialValues: any,
+  children: JSX.Element
+}
+
+function HydrateAtoms({ initialValues, children }: TestProviderProps) {
+  useHydrateAtoms(initialValues)
+  return children
+}
+
+function TestProvider({ initialValues, children }: TestProviderProps) {
+  return (
+    <JotaiProvider>
+      <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+    </JotaiProvider>
+  )
+}
+
+function SearchResultsError() {
+  return (
+    <TestProvider initialValues={[[searchResAtom, { users: [], error: 'error' }]]}>
+      <SearchResults />
+    </TestProvider>
+  )
+}
+
+function SearchResultsNoneFound() {
+  return (
+    <TestProvider initialValues={[[searchResAtom, { users: [] }]]}>
+      <SearchResults />
+    </TestProvider>
+  )
+}
+
+describe('Application renders', () => {
+  it('displays placeholder', () => {
+    customRender(<App />);
+    expect(screen.getByText(/Enter username/i))
+  });
+});
+
+describe('SearchResults communicates state', () => {
+  it('displays error', () => {
+    customRender(<SearchResultsError />);
+    expect(screen.getByText("error"))
+  })
+
+  it('shows "no users found" message', () => {
+    customRender(<SearchResultsNoneFound />);
+    expect(screen.getByText("No users matching your request"))
+  })
+})
+
+describe('Search uses debounce', () => {
+  it('waits before sending request', () => {
+    // ignore json type validation for now
+    //@ts-ignore
+    customRender(<Search />);
+    const input = screen.getByPlaceholderText("panda_coder")
+    userEvent.type(input, "test")
+    expect(currentSearchParams.query).toBe("")
+    setTimeout(() => {
+      expect(currentSearchParams.query).toBe("test")
+      userEvent.type(input, "test2")
+      expect(currentSearchParams.query).toBe("test")
+      userEvent.type(input, "test3")
+      setTimeout(() => {
+        expect(currentSearchParams.query).toBe("test3")
+      }, 250)
+    }, 250)
+  })
+})
+
---
+++ tests/test-utils.tsx
@@ -0,0 +1,12 @@
+import { cleanup, render } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
+afterEach(() => {
+  cleanup()
+})
+
+export const customRender = (element: React.ReactElement, opts = {}) =>
+  render(element, {
+    wrapper: ({ children }) => children,
+    ...opts,
+  })
